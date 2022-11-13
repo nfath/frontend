fe58dc9fbffb53da03adb56337096677c79335bd
Nader <nfath@yahoo.com>
2022-11-13 01:19:22 -0500

    exercise1 done!

---
+++ Session01/lab1/exercise1.js
@@ -0,0 +1,8 @@
+// 1st kind of function definition
+const lessThanOrEqualToZero = function(num) {
+    return num <= 0;
+}
+
+console.log(lessThanOrEqualToZero(5))
+console.log(lessThanOrEqualToZero(0))
+console.log(lessThanOrEqualToZero(-2))
