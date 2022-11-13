c9b4147a15348314d65a592e50236b11070edff4
Nader <nfath@yahoo.com>
2022-11-13 01:20:34 -0500

    function of exercise1 rewrittern with 2 othere styles!

--- Session01/lab1/exercise1.js
+++ Session01/lab1/exercise1.js
@@ -1,7 +1,15 @@
 // 1st kind of function definition
-const lessThanOrEqualToZero = function(num) {
-    return num <= 0;
-}
+// const lessThanOrEqualToZero = function(num) {
+//     return num <= 0;
+// }
+
+// 2st kind of function definition
+// const lessThanOrEqualToZero = num => {
+//     return num <= 0;
+// }
+
+// 3rd kind of function definition
+const lessThanOrEqualToZero = num => num <= 0;
 
 console.log(lessThanOrEqualToZero(5))
 console.log(lessThanOrEqualToZero(0))
