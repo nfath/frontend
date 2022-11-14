124357bc529a0eb551e05f363d36011f7334d0ad
Nader <nfath@yahoo.com>
2022-11-13 22:49:54 -0500

    findfacts function in exercise7 rewritten in 2 style more in ES6 syntax

--- Session01/lab1/exercise7.js
+++ Session01/lab1/exercise7.js
@@ -1,7 +1,15 @@
-const findFacts = (city) => {
-    let strText = city.name + " has a population of " + city.population + " and located in " + city.continent;
-    console.log(strText);
-}
+// const findFacts = (city) => {
+//     let strText = city.name + " has a population of " + city.population + " and located in " + city.continent;
+//     console.log(strText);
+// }
+
+// const findFacts = (city) => {
+//     // use this character for string: ` and it is different from '
+//     let strText = `${city.name} has a population of ${city.population} and located in ${city.continent}`;
+//     console.log(strText);
+// }
+
+const findFacts = (city) => console.log(`${city.name} has a population of ${city.population} and located in ${city.continent}`);
 
 findFacts({
     name: "Toronto",
